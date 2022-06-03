7e669cea363b5d31f8705f005d4d0449a6f33464
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-06-03 13:17:54 +0430

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+const Footer = () => {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        bottom: 0,
+      }}
+    >
+      <h2>Footer</h2>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Header = () => {
+        return (
+    <AppBar>
+      <Toolbar>
+      <IconButton>
+      <GitHubIcon />
+      </IconButton>
+
+        <h1>hello</h1>
+      </Toolbar>
+    </AppBar>
+                )
+}
+
+export default Header
