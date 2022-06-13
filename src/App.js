676825c75cb0268242c1b74b9f0bc4786b705ce5
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-06-14 00:40:56 +0430

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
 import List from "@mui/material/List";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 import UserResults from "./users/UserResults";
-import UserSearch from './users/UserSearch'
+import UserSearch from "./users/UserSearch";
 
 const App = () => {
-
   return (
-    <Box sx={{mt: {xs: '4em'}}}>
+    <Box sx={{ mt: { xs: "4em" } }}>
       <h1>Welcome</h1>
       Search for a github profile:
       <UserSearch />
