d01b7a379336ea0149eb7fe950602ab45492c987
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-07-28 10:16:08 +0430

    add about

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,9 +8,14 @@ const About = () => {
     <Box sx={{ mt: { xs: "4em" } }}>
       <h2>About Me</h2>
       <Typography>
-        Hello, My name is Esmaeil Saleh. Being a self-thought developer, I've
+        Hello, My name is Esmaeil Saleh. Being a self-thought developer, I have
         learn to code by Love and enthuisiasm.
       </Typography>
+      <p style={{display:'none'}}>
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+      </p>
     </Box>
   );
 };
